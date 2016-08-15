0774598dd7cc0dc5f3f9c8710cfbf4dc391518f8
Ashwinsunkunkari <ashwini_sunkunkari@epam.com>
2016-08-15 15:55:35 +0530

    check in

---
+++ ConsoleApplication1/ConsoleApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleApplication1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("EPAM Systems")>
+<Assembly: AssemblyProduct("ConsoleApplication1")>
+<Assembly: AssemblyCopyright("Copyright © EPAM Systems 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d4e03625-8fcf-4d5e-a67a-3cb2e62a2f6c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
